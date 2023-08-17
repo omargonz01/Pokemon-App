49517fbf98610a4f3cce6e3f4ed7e18e184547e9
Omar Gonzalez <gomarzalez@gmail.com>
2023-08-16 20:35:48 -0700

    build login and signup form no styles applied yet

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,20 @@
 from flask_wtf import FlaskForm 
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
 
 class PokemaneForm(FlaskForm):
     pokemane = StringField('Enter Pokemon Names or Numbers:', validators=[DataRequired()])
     submit_btn = SubmitField(('Submit'))
+
+class SignupForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit_btn = SubmitField(('Register'))
+
+class LoginForm(FlaskForm):
+    email = EmailField('Email Address: ', validators=[DataRequired()])
+    password = PasswordField('Password: ', validators=[DataRequired()])
+    submit_btn = SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,49 @@
 from flask import request, render_template
 import requests
 from app import app
-from .forms import PokemaneForm
+from .forms import PokemaneForm, SignupForm, LoginForm
 
 @app.route("/")
 @app.route("/home")
 def home():
     return 'Hello User, Glad You Could Join Us'
+
+# temp database to test
+TEST_USERS = {
+    'sk8andthra5h@gmail.com': {
+        'name': 'OG',
+        'password': 'killawhale1'
+    }
+}
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        name = f'{form.first_name.data} {form.last_name.data}'
+        email = form.email.data
+        password = form.password.data
+        TEST_USERS[email] = {
+            'name': name,
+            'password': password
+        }
+        return f'Thanks for testing {name}, we see you!'
+    else:
+        return render_template('signup.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data.lower()
+        password = form.password.data
+        if email in TEST_USERS and password == TEST_USERS[email]['password']:
+            return f'Hello, {TEST_USERS[email]["name"]}, thanks for coming back!'
+        else:
+            return 'Invalid email or password, try again playa'
+    else:
+        print('not valid, try again')
+        return render_template('login.html', form=form)
 
 @app.route("/pokemon", methods=['GET', 'POST'])
 def get_pokemanes():
